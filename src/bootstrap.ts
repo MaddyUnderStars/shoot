56f6858728c81374fe19d59f5f8ec062e0f2f9d2
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-12-10 12:41:58 +1100

    node version check

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -8,8 +8,6 @@
 	- Config validation
 	-	- including activitypub config
 
-	- Nodejs version validation
-
 	- Maybe a cute ascii logo :3
 
 	- Then start server
@@ -20,6 +18,14 @@ import { createLogger } from "./util";
 
 const Log = createLogger("bootstrap");
 Log.msg("Starting")
+
+// Check nodejs version
+const NODE_REQUIRED_VERSION = 18;
+const [NODE_MAJOR_VERSION] = process.versions.node.split(".").map(Number);
+if (NODE_MAJOR_VERSION < NODE_REQUIRED_VERSION) {
+	Log.error(`You are running node version ${NODE_MAJOR_VERSION}. We require a version > ${NODE_REQUIRED_VERSION}. Please upgrade.`);
+	process.exit(1);
+}
 
 const chatServer = new ChatServer();
 
