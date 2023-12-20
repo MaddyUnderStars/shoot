509cd691d36d84fb0843f2959abc3cd09effeb65
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-12-20 08:42:12 +0000

    bleh

--- src/http/s2s/index.ts
+++ src/http/s2s/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { z } from "zod";
 import { User } from "../../entity";
 import { addContext, config, route } from "../../util";
+import { makeOrderedCollection } from "../../util/activitypub/orderedCollection";
 
 const router = Router();
 
@@ -47,5 +48,59 @@ router.get(
 router.post("/users/:user_id/inbox", (req, res) => {
 	console.log("yay", req.body);
 });
+
+router.get(
+	"/users/:user_id/outbox",
+	route(
+		{
+			query: z.object({
+				page: z.boolean({ coerce: true }).optional(),
+				min_id: z.string().optional(),
+				max_id: z.string().optional(),
+			}),
+		},
+		async (req, res) => {
+			return res.json(
+				await makeOrderedCollection({
+					page: req.query.page ?? false,
+					min_id: req.query.min_id,
+					max_id: req.query.max_id,
+					id: `${config.federation.instance_url.origin}${req.originalUrl}`,
+					getElements: async () => {
+						return [];
+					},
+					getTotalElements: async () => 0,
+				}),
+			);
+		},
+	),
+);
+
+router.get(
+	"/users/:user_id/followers",
+	route(
+		{
+			query: z.object({
+				page: z.boolean({ coerce: true }).optional(),
+				min_id: z.string().optional(),
+				max_id: z.string().optional(),
+			}),
+		},
+		async (req, res) => {
+			return res.json(
+				await makeOrderedCollection({
+					page: req.query.page ?? false,
+					min_id: req.query.min_id,
+					max_id: req.query.max_id,
+					id: `${config.federation.instance_url.origin}${req.originalUrl}`,
+					getElements: async () => {
+						return [];
+					},
+					getTotalElements: async () => 0,
+				}),
+			);
+		},
+	),
+);
 
 export default router;
---
+++ src/util/activitypub/orderedCollection.ts
@@ -0,0 +1,43 @@
+import { APOrderedCollection, CollectionCurrentField } from "activitypub-types";
+import { ACTIVITYSTREAMS_CONTEXT } from ".";
+
+export const makeOrderedCollection = async <
+	T extends CollectionCurrentField,
+>(opts: {
+	page: boolean;
+	min_id?: string;
+	max_id?: string;
+	id: string;
+	getTotalElements: () => Promise<number>;
+	getElements: (before?: string, after?: string) => Promise<T[]>;
+}): Promise<APOrderedCollection> => {
+	const { page, min_id, max_id, id, getTotalElements, getElements } = opts;
+
+	if (!page)
+		return {
+			"@context": ACTIVITYSTREAMS_CONTEXT,
+			id: id,
+			type: "OrderedCollection",
+			totalItems: await getTotalElements(),
+			first: new URL(`${id}?page=true`),
+			last: new URL(`${id}?page=true&min_id=0`),
+		};
+
+	const after = min_id ? `${min_id}` : undefined;
+	const before = max_id ? `${max_id}` : undefined;
+
+	const elems = await getElements(before, after);
+
+	// TODO: we need to specify next/prev props
+	// and should probably let the caller of this function specify what they are
+	// along with first/last
+	return {
+		"@context": ACTIVITYSTREAMS_CONTEXT,
+		id: `${id}?page=true`,
+		type: "OrderedCollection",
+		first: new URL(`${id}?page=true`),
+		last: new URL(`${id}?page=true&min_id=0`),
+		totalItems: await getTotalElements(),
+		orderedItems: elems,
+	};
+};
