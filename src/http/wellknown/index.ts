fdc58dc7fe78a402324a500465aa4885714ed8a8
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-12-26 08:00:40 +0000

    node-info

--- src/http/wellknown/index.ts
+++ src/http/wellknown/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import hostMeta from "./host-meta";
+import nodeInfo from "./node-info";
 import webfinger from "./webfinger";
 
 const router = Router();
 
-router.use("/.well-known", hostMeta, webfinger);
+router.use("/.well-known", hostMeta, webfinger, nodeInfo);
 
 export default router;
