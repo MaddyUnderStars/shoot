d9f793cc8ea46fe8ce9f185eaa98a987c8b3675c
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2024-02-21 08:20:37 +0000

    delete session on gateway connection close

--- src/gateway/socket/close.ts
+++ src/gateway/socket/close.ts
@@ -1,13 +1,14 @@
 import { CloseEvent } from "ws";
+import { Session } from "../../entity";
 import { createLogger } from "../../util";
 import { Websocket } from "../util/websocket";
 
 const Log = createLogger("gateway");
 
-export function onClose(this: Websocket, event: CloseEvent) {
+export async function onClose(this: Websocket, event: CloseEvent) {
 	Log.verbose(`${this.ip_address} disconnected with code ${event.code}`);
 
 	clearTimeout(this.auth_timeout);
 
-	// TODO: delete the session
+	await Session.delete({ id: this.session.id });
 }
