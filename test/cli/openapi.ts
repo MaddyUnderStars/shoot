432a948bad56a3ca73ceb74e0184327194270d49
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2024-06-04 08:48:18 +0000

    fix test?

--- test/cli/openapi.ts
+++ test/cli/openapi.ts
@@ -1,13 +1,25 @@
 import test from "ava";
 import { spawn } from "child_process";
 import fs from "fs";
+import path from "path";
 
 test("Can generate openapi schema", (t) =>
 	new Promise((resolve, reject) => {
 		const child = spawn("npm", ["run", "openapi"]);
 		child.on("close", (code) => {
 			t.is(code, 0);
-			t.assert(fs.existsSync("./assets/client.json"));
+			t.assert(
+				fs.existsSync(
+					path.join(
+						__dirname,
+						"..",
+						"..",
+						"..",
+						"assets",
+						"client.json",
+					),
+				),
+			);
 			resolve();
 		});
 	}));
