7d16e56c5fe0163c3ee843d5b7904dfd103ff3d1
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2024-07-24 06:58:08 +0000

    channel fetching test

---
+++ test/http/channels.ts
@@ -0,0 +1,50 @@
+import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
+import { z } from "zod";
+extendZodWithOpenApi(z);
+
+import test from "ava";
+import { createTestDm, createTestUser, setupTests } from "../helpers";
+setupTests(test);
+
+import request from "supertest";
+
+test("Get local", async (t) => {
+	const { APIServer } = await import("../../src/http/server");
+	const api = new APIServer();
+
+	const [user1] = await Promise.all([
+		createTestUser("local1"),
+		createTestUser("local2"),
+	]);
+	const dm = await createTestDm("localdm", "local1@localhost", [
+		"local2@localhost",
+	]);
+
+	const ret = await request(api.app)
+		.get(`/channel/${dm.mention}`)
+		.auth(user1, { type: "bearer" })
+		.expect(200);
+
+	t.deepEqual(ret.body, dm.toPublic());
+});
+
+test("Get local as non-member", async (t) => {
+	const { APIServer } = await import("../../src/http/server");
+	const api = new APIServer();
+
+	const [, , user3] = await Promise.all([
+		createTestUser("local3"),
+		createTestUser("local4"),
+		createTestUser("local5"),
+	]);
+	const dm = await createTestDm("localdm", "local3@localhost", [
+		"local4@localhost",
+	]);
+
+	const ret = await request(api.app)
+		.get(`/channel/${dm.mention}`)
+		.auth(user3, { type: "bearer" })
+		.expect(400);
+
+	t.pass();
+});
