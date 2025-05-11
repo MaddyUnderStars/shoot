6c8b412b09da2599d81664f11e9907f23f28c54c
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-05-11 06:13:30 +0000

    feat: fetch remote messages in GET /channel/:id/messages/:id

--- src/http/api/channel/#id/messages/#id/index.ts
+++ src/http/api/channel/#id/messages/#id/index.ts
@@ -1,7 +1,15 @@
+import { ObjectIsNote } from "activitypub-types";
 import { Router } from "express";
 import { z } from "zod";
 import { Message, PublicMessage } from "../../../../../../entity";
-import { PERMISSION, getOrFetchChannel, route } from "../../../../../../util";
+import {
+	APError,
+	PERMISSION,
+	buildMessageFromAPNote,
+	getOrFetchChannel,
+	resolveAPObject,
+	route,
+} from "../../../../../../util";
 
 const router = Router({ mergeParams: true });
 
@@ -20,9 +28,7 @@ router.get(
 
 			await channel.throwPermission(req.user, [PERMISSION.VIEW_CHANNEL]);
 
-			// TODO: fetch remote messages
-
-			const message = await Message.findOneOrFail({
+			let message = await Message.findOne({
 				where: {
 					channel: { id: channel.id },
 					id: req.params.message_id,
@@ -32,6 +38,32 @@ router.get(
 					disableMixedMap: true,
 				},
 			});
+
+			if (!message && channel.isRemote()) {
+				// If this is a remote channel, fetch the message from them
+				// The remote message may be cached
+
+				// TODO: this has an obvious flaw, you can't do webfinger for messages
+				// so we need to just guess the remote message URL
+				// and that will only realistically work for shoot
+				// although I suppose if you're calling this method, you likely already discovered the message
+				// through GET /channel/:id/messages and so the message is already in cache and would've
+				// been hit above. so probably fine?
+				// If you could filter a channels outbox somehow, that would be maybe more portable
+				const messageUrl = `${channel.remote_address}/message/${req.params.message_id}`;
+				const obj = await resolveAPObject(messageUrl);
+
+				if (!ObjectIsNote(obj)) {
+					throw new APError("Remote did not return a Note object");
+				}
+
+				message = await buildMessageFromAPNote(obj, channel);
+				await message.save();
+			}
+
+			if (!message) {
+				return res.sendStatus(404);
+			}
 
 			return res.json(message.toPublic());
 		},
