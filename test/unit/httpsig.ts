59a2ed4a929d059e2b5b18b41c14ce34aab2b938
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-06-25 08:48:56 +0000

    test: httpsig with activity

--- test/unit/httpsig.ts
+++ test/unit/httpsig.ts
@@ -2,6 +2,7 @@ import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 import { z } from "zod";
 extendZodWithOpenApi(z);
 
+import type { APActivity } from "activitypub-types";
 import test from "ava";
 import { setupTests } from "../helpers/env";
 setupTests(test);
@@ -33,6 +34,46 @@ test("Using Instance Actor", async (t) => {
 		"GET",
 		//@ts-ignore
 		signed.headers,
+	);
+
+	t.pass();
+});
+
+test("Using Instance Actor with Activity", async (t) => {
+	const { signWithHttpSignature, validateHttpSignature } = await import(
+		"../../src/util/activitypub/httpsig"
+	);
+	const { User } = await import("../../src/entity/user");
+	const { InstanceActor } = await import(
+		"../../src/util/activitypub/instanceActor"
+	);
+
+	const actor = await User.create({
+		...InstanceActor,
+		name: "remote_user",
+		remote_address: "http://localhost/users/remote_user",
+		id: undefined,
+	}).save();
+
+	const activity: APActivity = {
+		id: "http://localhost/test_activity",
+		attributedTo: "http://localhost/users/remote_user",
+		type: "Test",
+	};
+
+	const signed = signWithHttpSignature(
+		"https://chat.understars.dev/inbox",
+		"GET",
+		actor,
+		JSON.stringify(activity),
+	);
+
+	await validateHttpSignature(
+		"/inbox",
+		"GET",
+		//@ts-ignore
+		signed.headers,
+		JSON.stringify(activity),
 	);
 
 	t.pass();
