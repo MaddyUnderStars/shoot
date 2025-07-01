2d286043be45465532e6e3bc42229058bd904fc4
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-07-01 04:34:05 +0000

    feat: add instance url to our user agent. close #64

--- src/util/activitypub/constants.ts
+++ src/util/activitypub/constants.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { config } from "../config";
 
 export const ACTIVITYSTREAMS_CONTEXT = "https://www.w3.org/ns/activitystreams";
 
@@ -8,7 +9,7 @@ export const ACTIVITY_JSON_ACCEPT = [
 	"application/activity+json",
 ];
 
-export const USER_AGENT = "Shoot (https://github.com/maddyunderstars/shoot)";
+export const USER_AGENT = `Shoot (https://github.com/maddyunderstars/shoot; +${config.federation.webapp_url.origin})`;
 
 export const ACTIVITYPUB_FETCH_OPTS: RequestInit = {
 	headers: {
