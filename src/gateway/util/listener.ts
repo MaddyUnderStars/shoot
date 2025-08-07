MaddyUnderStars/shoot

commit 35d955c10bd97150a41c2cd748f2c6deb94bb5b8

fix: prevent gateway event target collisions. close #84

src/gateway/util/listener.ts
@@ -1,6 +1,8 @@
+import type { BaseModel } from "../../entity/basemodel";
+import { User } from "../../entity/user";
 import { splitQualifiedMention } from "../../util/activitypub/util";
 import { channelInGuild } from "../../util/entity/channel";
-import { listenGatewayEvent } from "../../util/events";
+import { listenGatewayEvent, makeGatewayTarget } from "../../util/events";
 import { createLogger } from "../../util/log";
 import { handleMemberListRoleAdd } from "../handlers/members";
 import type { GATEWAY_EVENT } from "./validation/send";
@@ -16,14 +18,16 @@ const Log = createLogger("GATEWAY:LISTENER");
  */
 export const listenEvents = (
 	socket: Websocket,
-	emitters: string[],
+	emitters: BaseModel[],
 	callback = consume,
 ) => {
 	for (const emitter of emitters) {
-		if (socket.events[emitter])
+		const id = makeGatewayTarget(emitter);
+
+		if (socket.events[id])
 			Log.warn(`${socket.user_id} is already listening to ${emitter}`);
 
-		socket.events[emitter] = listenGatewayEvent(emitter, (payload) =>
+		socket.events[id] = listenGatewayEvent(emitter, (payload) =>
 			callback(socket, payload),
 		);
 	}
@@ -45,12 +49,12 @@ export const consume = async (socket: Websocket, payload: GATEWAY_EVENT) => {
 
 		case "CHANNEL_CREATE": {
 			const { user } = splitQualifiedMention(payload.channel.mention);
-			listenEvents(socket, [user]);
+			listenEvents(socket, [User.create({ id: user })]);
 			break;
 		}
 		case "GUILD_CREATE": {
 			const { user } = splitQualifiedMention(payload.guild.mention);
-			listenEvents(socket, [user]);
+			listenEvents(socket, [User.create({ id: user })]);
 			break;
 		}
 		case "GUILD_DELETE":

src/gateway/handlers/identify.ts
@@ -51,11 +51,11 @@ export const onIdentify = makeHandler(async function (payload) {
 	this.session = session;
 
 	listenEvents(this, [
-		this.user_id,
+		user,
 		// TODO: for relationships, see #54
-		...dmChannels.map((x) => x.id),
-		...guilds.map((x) => x.id),
-		...guilds.flatMap((x) => x.channels.map((y) => y.id)),
+		...dmChannels,
+		...guilds,
+		...guilds.flatMap((x) => x.channels),
 	]);
 
 	const ret: READY = {

src/gateway/handlers/members.ts
@@ -1,5 +1,6 @@
 import { makeHandler } from ".";
 import { DMChannel } from "../../entity/DMChannel";
+import { Member } from "../../entity/member";
 import { GuildTextChannel } from "../../entity/textChannel";
 import { User } from "../../entity/user";
 import type { ActorMention } from "../../util/activitypub/constants";
@@ -145,21 +146,21 @@ export const handleMemberListRoleAdd = async (
  */
 const listenRangeEvent = async (
 	socket: Websocket,
-	target_id: string,
+	member_id: string,
 	channel_id: string,
 ) => {
-	if (socket.member_list.events[target_id]) return;
-	if (socket.user_id === target_id) return;
+	if (socket.member_list.events[member_id]) return;
+	if (socket.user_id === member_id) return;
 
 	const unsubscribe = () => {
-		socket.member_list.events[target_id]();
-		delete socket.member_list.events[target_id];
+		socket.member_list.events[member_id]();
+		delete socket.member_list.events[member_id];
 	};
 
 	// TODO: don't we also want to subscribe to the user id?
 
-	socket.member_list.events[target_id] = listenGatewayEvent(
-		target_id,
+	socket.member_list.events[member_id] = listenGatewayEvent(
+		Member.create({ id: member_id }),
 		async (payload) => {
 			// Listening to a member id is intended to only send requests about that guild member
 			// And so we just need to track the events that move their position within the list
@@ -172,7 +173,7 @@ const listenRangeEvent = async (
 					const position = await getMemberPosition(
 						channel_id,
 						...(socket.member_list.range ?? [0, 100]),
-						target_id,
+						member_id,
 					);
 
 					if (!position) {

src/http/api/channel/#id/index.ts
@@ -62,9 +62,7 @@ router.patch(
 			await Channel.update({ id: channel.id }, req.body);
 
 			emitGatewayEvent(
-				channel instanceof GuildTextChannel
-					? channel.guild.id
-					: channel.id,
+				channel instanceof GuildTextChannel ? channel.guild : channel,
 				{
 					type: "CHANNEL_UPDATE",
 					channel: channel.toPublic(),
@@ -89,7 +87,7 @@ router.delete(
 		await channel.throwPermission(req.user, PERMISSION.MANAGE_CHANNELS);
 
 		// after a .remove, the id is made undefined but other properties are not
-		emitGatewayEvent(channel.id, {
+		emitGatewayEvent(channel, {
 			type: "CHANNEL_DELETE",
 			channel: channel.mention,
 			guild:

src/http/api/channel/#id/messages/#id/index.ts
@@ -110,7 +110,7 @@ router.delete(
 					PERMISSION.MANAGE_MESSAGES,
 				]);
 
-			emitGatewayEvent(channel.id, {
+			emitGatewayEvent(channel, {
 				type: "MESSAGE_DELETE",
 				message_id: message.id,
 				channel: channel.mention,

src/http/api/guild/#id/index.ts
@@ -67,7 +67,7 @@ router.patch(
 			guild.assign(req.body);
 			await Guild.update({ id: guild.id }, req.body);
 
-			emitGatewayEvent(guild.id, {
+			emitGatewayEvent(guild, {
 				type: "GUILD_UPDATE",
 				guild: guild.toPublic(),
 			});
@@ -88,7 +88,7 @@ router.delete(
 
 			await guild.throwPermission(req.user, PERMISSION.ADMIN);
 
-			emitGatewayEvent(guild.id, {
+			emitGatewayEvent(guild, {
 				type: "GUILD_DELETE",
 				guild: guild.mention,
 			});
@@ -129,7 +129,7 @@ router.post(
 
 			await Invite.insert(invite);
 
-			emitGatewayEvent(guild_id, {
+			emitGatewayEvent(guild, {
 				type: "INVITE_CREATE",
 				invite: invite.toPublic(),
 			});

src/http/api/users/#id/relationship.ts
@@ -97,12 +97,12 @@ router.delete(
 
 			// tell both parties that we've deleted the relationship
 			// can't do it in one call, unfortunately. without some hackery of course
-			emitGatewayEvent(req.user.id, {
+			emitGatewayEvent(req.user, {
 				type: "RELATIONSHIP_DELETE",
 				user: to.mention,
 			});
 
-			emitGatewayEvent(to.id, {
+			emitGatewayEvent(to, {
 				type: "RELATIONSHIP_DELETE",
 				user: req.user.mention,
 			});

src/http/api/users/@me/guild.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { z } from "zod";
-import { PublicGuild } from "../../../../entity/guild";
+import { Guild, PublicGuild } from "../../../../entity/guild";
 import { Member } from "../../../../entity/member";
 import { splitQualifiedMention } from "../../../../util/activitypub/util";
 import { getDatabase } from "../../../../util/database";
@@ -67,7 +67,7 @@ router.delete(
 			.execute();
 
 		if (deleted.affected) {
-			emitGatewayEvent(req.params.guild_id, {
+			emitGatewayEvent(Guild.create({ id: req.params.guild_id }), {
 				type: "MEMBER_LEAVE",
 				guild: `${mention.user}@${mention.domain}`,
 				user: req.user.mention,

commit ff1893bb3845855524a6a13e1973440e5017df32

fix: regression caused by 35d955c10bd97150a41c2cd748f2c6deb94bb5b8
the mentioned commit fixes #84, a bug where gateway targets could becoming listening to other targets events
if they share the same ID. There was an error made in the migration for this change, where
CHANNEL_CREATE and GUILD_CREATE events were being treated as listens for user objects, not the respective channel or guild
objects. The confusion was due to the return type of splitQualifiedMention containing a 'user' key.

src/gateway/util/listener.ts
@@ -1,5 +1,6 @@
 import type { BaseModel } from "../../entity/basemodel";
-import { User } from "../../entity/user";
+import { Channel } from "../../entity/channel";
+import { Guild } from "../../entity/guild";
 import { splitQualifiedMention } from "../../util/activitypub/util";
 import { channelInGuild } from "../../util/entity/channel";
 import { listenGatewayEvent, makeGatewayTarget } from "../../util/events";
@@ -49,12 +50,12 @@ export const consume = async (socket: Websocket, payload: GATEWAY_EVENT) => {
 
 		case "CHANNEL_CREATE": {
 			const { user } = splitQualifiedMention(payload.channel.mention);
-			listenEvents(socket, [User.create({ id: user })]);
+			listenEvents(socket, [Channel.create({ id: user })]);
 			break;
 		}
 		case "GUILD_CREATE": {
 			const { user } = splitQualifiedMention(payload.guild.mention);
-			listenEvents(socket, [User.create({ id: user })]);
+			listenEvents(socket, [Guild.create({ id: user })]);
 			break;
 		}
 		case "GUILD_DELETE":