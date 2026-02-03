ab6d256f94c65e36f55009fe3ccf167ee97272e3
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-02-03 17:03:31 +1100

    feat: json schema generation for configs

---
+++ src/scripts/configSchema.ts
@@ -0,0 +1,45 @@
+import { writeFile } from "node:fs";
+import path from "node:path";
+import {
+	extendZodWithOpenApi,
+	OpenAPIRegistry,
+	OpenApiGeneratorV31,
+} from "@asteasolutions/zod-to-openapi";
+import z from "zod";
+import { ConfigSchema } from "../util/ConfigSchema";
+
+extendZodWithOpenApi(z);
+
+const registry = new OpenAPIRegistry();
+
+registry.register("Config", ConfigSchema);
+
+const generator = new OpenApiGeneratorV31(registry.definitions);
+
+const document = generator.generateDocument({
+	openapi: "3.1.0",
+	info: {
+		version: "1.0.0",
+		title: "ConfigSchema",
+	},
+});
+
+const configSchema = {
+	$schema: "https://json-schema.org/draft/2020-12/schema",
+
+	// biome-ignore lint/style/noNonNullAssertion: .
+	...document.components!.schemas!.Config,
+};
+
+writeFile(
+	path.join(__dirname, "..", "..", "assets", "config.json"),
+	JSON.stringify(configSchema),
+	{},
+	(err) => {
+		if (err) console.error(err);
+		else {
+			console.log("done");
+			process.exit();
+		}
+	},
+);
