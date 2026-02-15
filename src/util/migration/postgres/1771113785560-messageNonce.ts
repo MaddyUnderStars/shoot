37d32082f121338d39b31a563fd382eb3909b45e
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-02-15 11:26:36 +1100

    fix: name of migration...

--- src/util/migration/postgres/1771113785560-messageNonce.ts
+++ src/util/migration/postgres/1771113785560-messageNonce.ts
@@ -1,6 +1,8 @@
 import type { MigrationInterface, QueryRunner } from "typeorm";
 
 export class MessageNonce1771113785560 implements MigrationInterface {
+	name = "MessageNonce1771113785560";
+
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
 			`ALTER TABLE "messages" ADD COLUMN "nonce" uuid DEFAULT NULL`,
