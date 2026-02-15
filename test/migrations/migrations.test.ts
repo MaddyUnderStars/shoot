41eac05088ac8283f3721ebc8c12a6172a0abdf9
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-02-15 22:11:11 +1100

    test: migrations. close #123

---
+++ test/migrations/migrations.test.ts
@@ -0,0 +1,36 @@
+import { describe } from "vitest";
+import { test } from "../fixture";
+
+describe("Migrations", () => {
+	test.sequential("Can rollback migrations from initial sync", async ({
+		annotate,
+	}) => {
+		const { getDatabase, initDatabase } = await import(
+			"../../src/util/database"
+		);
+
+		await initDatabase();
+
+		const db = getDatabase();
+		const qr = db.createQueryRunner();
+		const migrations = db.migrations;
+
+		for (const migration of migrations.reverse()) {
+			await annotate(migration.name ?? "unknown name");
+			await migration.down(qr);
+		}
+	});
+
+	test.sequential("Can apply migrations", async ({ annotate }) => {
+		const { getDatabase } = await import("../../src/util/database");
+
+		const db = getDatabase();
+		const qr = db.createQueryRunner();
+		const migrations = db.migrations;
+
+		for (const migration of migrations.reverse()) {
+			await annotate(migration.name ?? "unknown name");
+			await migration.up(qr);
+		}
+	});
+});
