3d0e60250023f913c6288cbf7fdaf87b8904c675
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-05-10 19:17:36 +1000

    fix: tests

--- test/testUtils/cli.ts
+++ test/testUtils/cli.ts
@@ -6,7 +6,7 @@ export const runCliInContainer = async (
 ) => {
 	const split = Array.isArray(command) ? command : command.split(" ");
 
-	const res = await container.exec(["/nodejs/bin/node", "/app/dist/cli/index.js", ...split]);
+	const res = await container.exec(["/nodejs/bin/node", "/app/dist/src/cli/index.js", ...split]);
 
 	if (res.exitCode !== 0) throw new Error(res.stderr);
 
